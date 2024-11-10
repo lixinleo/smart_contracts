a8e392d370224d07a1d93c2f9dd47a4dce7fcc97
lixinleo <lixinebo@yahoo.ca>
2024-11-09 22:49:36 -0800

    add some

---
+++ MyContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+contract MyContract {
+    string public ourString = "hello world";
+
+    function updateOurString(string memory _updateString) public {
+        ourString = _updateString;
+    }
+}
