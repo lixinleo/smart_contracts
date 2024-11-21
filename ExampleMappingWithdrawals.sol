d6d7b9d1536601960c76390a9fdab45f91e68ad7
lixinleo <lixinebo@yahoo.ca>
2024-11-20 20:58:44 -0800

    send money

---
+++ ExampleMappingWithdrawals.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+contract ExampleMappingWithdrawals {
+    mapping(address => uint) public balanceReceived;
+
+    function setMoney() public payable {
+        balanceReceived[msg.sender] += msg.value;
+    }
+
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        uint balanceToSend = balanceReceived[msg.sender];
+        balanceReceived[msg.sender] = 0;
+        _to.transfer(balanceToSend);
+    }
+}
