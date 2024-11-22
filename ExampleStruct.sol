a96b6209785604324f788e8489d4221743297935
lixinleo <lixinebo@yahoo.ca>
2024-11-21 21:10:44 -0800

    use struct

--- ExampleStruct.sol
+++ ExampleStruct.sol
@@ -2,19 +2,14 @@
 pragma solidity ^0.8.15;
 
 contract Wallet {
+    struct PaymentReceived {
+        address from;
+        uint amount;
+    }
+
     PaymentReceived public payment;
 
     function payContract() public payable {
-        payment = new PaymentReceived(msg.sender, msg.value);
-    }
-}
-
-contract PaymentReceived {
-    address public from;
-    uint public amount;
-
-    constructor(address _from, uint _amount) {
-        from = _from;
-        amount = _amount;
+        payment = PaymentReceived({from: msg.sender, amount: msg.value});
     }
 }
